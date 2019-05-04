ff7dc35a6eb0530ac306fa9b99421c00c3657d28
123rainbow123 <1306349650@qq.com>
2019-05-04 21:50:15 +0800

    添加产品说明

---
+++ files/产品说明（图片）/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="产品说明（图片）.html",d="generationDate",e=new Date(1556977734175.85),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="5f21bdb04409479a8195cea0bdce0c4b",q="type",r="Axure:Page",s="name",t="产品说明（图片）",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFF32608A,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="8ee2175d5d924eb2a64d79d1cccb773b",Y="label",Z="friendlyType",ba="图片",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=940,bj="height",bk=3570,bl="imageOverrides",bm="images",bn="normal~",bo="images/产品说明（图片）/u1181.png",bp="masters",bq="objectPaths",br="8ee2175d5d924eb2a64d79d1cccb773b",bs="scriptId",bt="u1181";
+return _creator();
+})());
